57b140b1ebe9e6893468ce4e59147b7eac4a12d0
Matheus Sena <senamatheusjob@gmail.com>
2024-11-01 19:57:45 -0300

    feat: Add health check endpoint to ExpressAdapter

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { ExpressAdapter } from './shared/infra/http/ExpressAdapter';
 
 const httpServer = new ExpressAdapter();
+httpServer.register('get', '/health', async () => {
+    return { status: 'UP' }
+})
 httpServer.listen(5000);
