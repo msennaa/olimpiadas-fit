933a3731fc197489cbc202298b5ec01e3a856da3
Matheus Sena <senamatheusjob@gmail.com>
2024-11-01 20:18:49 -0300

    feat: Add finishCompetition method to Competition class

--- src/Competition/domain/entity/Competition.ts
+++ src/Competition/domain/entity/Competition.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
 export default class Competition {
-    constructor(readonly id: string, readonly name: string, readonly status: string, readonly competitionTypeId: string, readonly startCompetition: Date, readonly endCompetition: Date | null) {
+    constructor(readonly id: string, readonly name: string, public status: string, readonly competitionTypeId: string, readonly startCompetition: Date, public endCompetition: Date | null) {
 
     }
 
@@ -11,5 +11,11 @@ export default class Competition {
         const startCompetition = new Date();
         const endCompetition = null;
         return new Competition(competitionId, name, status, competitionTypeId, startCompetition, endCompetition);
+    }
+
+    finishCompetition(): void {
+        if (this.status !== 'in-progress') throw new Error('Competition is not in progress');
+        this.status = 'finished';
+        this.endCompetition = new Date();
     }
 }
