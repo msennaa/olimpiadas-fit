35f4f48e9937ba50f5ecaaccb5da53bd21838ce8
Matheus Sena <senamatheusjob@gmail.com>
2024-11-01 19:46:23 -0300

    feat: Add HttpServer interface and ExpressAdapter implementation

---
+++ src/shared/infra/http/ExpressAdapter.ts
@@ -0,0 +1,28 @@
+import HttpServer from '../../application/http/HttpServer';
+import express from 'express';
+
+export class ExpressAdapter implements HttpServer {
+    app: any
+
+    constructor() {
+        this.app = express();
+        this.app.use(express.json());
+    }
+
+    register(method: string, url: string, callback: Function): void {
+        this.app[method](url.replace(/\{|\}/g, ''), async (req: any, res: any) => {
+            try {
+                const output = await callback(req.params, req.body);
+                res.json(output);
+            } catch (error: any) {
+                res.status(422).json({
+                    message: error.message
+                })
+            }
+        });
+    }
+
+    listen(port: number): void {
+        this.app.listen(port);
+    }
+}
