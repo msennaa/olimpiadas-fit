6d6590ef386e730d158220c7d55b34f39ca2e073
Matheus Sena <senamatheusjob@gmail.com>
2024-11-01 20:40:26 -0300

    Refactor status test descriptions for clarity

--- test/Competition/unit/Status.test.ts
+++ test/Competition/unit/Status.test.ts
@@ -3,7 +3,7 @@ import Status from '../../../src/Competition/domain/entity/Status';
 test.each([
     'in-progress',
     'finished',
-])("Deve testar se o status é válido %s", function (status: string) {
+])("Should test if status is invalid %s", function (status: string) {
     expect(new Status(status)).toBeDefined();
 });
 
@@ -14,6 +14,6 @@ test.each([
     "any_status",
     "12345678901234567890",
     "11111111111"
-])("Deve testar se o status é inválido %s", function (status: any) {
+])("Should test if status is invalid %s", function (status: any) {
     expect(() => new Status(status)).toThrow(new Error('Invalid status'))
 });
