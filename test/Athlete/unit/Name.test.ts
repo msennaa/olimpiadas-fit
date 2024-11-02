3b177a70edea73e7066bc8410283c1958f56e365
Matheus Sena <senamatheusjob@gmail.com>
2024-11-02 00:08:55 -0300

    feat: Add unit tests for Name value object in Athlete domain

---
+++ test/Athlete/unit/Name.test.ts
@@ -0,0 +1,18 @@
+import Name from '../../../src/Athlete/domain/vo/Name';
+
+test.each([
+    'Any Name',
+    'Another Name',
+    'Valid Name'
+])('Should test if name %s is valid', function (name) {
+    expect(new Name(name)).toBeDefined();
+})
+
+test.each([
+    '',
+    'name_with_special_characters_!@#$%¨&*()_+',
+    '12345678901234567890',
+    '11111111111'
+])('Should test if name %s is invalid', function (name) {
+    expect(() => new Name(name)).toThrow('Invalid name')
+});
