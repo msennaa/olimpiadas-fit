7e3fadefaf38dd60624dccc0b6a2bbdc8c3a750d
Matheus Sena <senamatheusjob@gmail.com>
2024-11-02 10:42:26 -0300

    feat: Add CompetitionTypeController and CreateCompetitionType use case

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import AthleteController from './Athlete/infra/controller/AthleteController';
 import RegisterAthlete from './Athlete/application/usecase/RegisterAthlete';
 import AthleteRepositoryDatabase from './Athlete/infra/repository/AthleteRepositoryDatabase';
 import { PgPromiseAdapter } from './shared/infra/database/PgPromiseAdapter';
+import CompetitionTypeController from './Competition/infra/controller/CompetitionTypeController';
+import CreateCompetitionType from './Competition/application/usecase/CreateCompetitionType';
+import CompetitionRepositoryTypeDatabase from './Competition/infra/repository/CompetitionTypeRepositoryDatabase';
 
 const httpServer = new ExpressAdapter();
 httpServer.register('get', '/health', async () => {
@@ -12,4 +15,7 @@ const connection = new PgPromiseAdapter();
 const athleteRepository = new AthleteRepositoryDatabase(connection);
 const registerAthlete = new RegisterAthlete(athleteRepository)
 new AthleteController(httpServer, registerAthlete);
+const competitionTypeRepository = new CompetitionRepositoryTypeDatabase(connection);
+const createCompetitionType = new CreateCompetitionType(competitionTypeRepository);
+new CompetitionTypeController(httpServer, createCompetitionType)
 httpServer.listen(5000);
