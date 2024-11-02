b9d989bbc7583af4f7651633c76a031b185eb34d
Matheus Sena <senamatheusjob@gmail.com>
2024-11-02 00:07:14 -0300

    feat: Add AthleteController and RegisterAthlete use case

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { ExpressAdapter } from './shared/infra/http/ExpressAdapter';
+import AthleteController from './Athlete/infra/controller/AthleteController';
+import RegisterAthlete from './Athlete/application/usecase/RegisterAthlete';
+import AthleteRepositoryDatabase from './Athlete/infra/repository/AthleteRepositoryDatabase';
+import { PgPromiseAdapter } from './shared/infra/database/PgPromiseAdapter';
 
 const httpServer = new ExpressAdapter();
 httpServer.register('get', '/health', async () => {
     return { status: 'UP' }
 })
+const connection = new PgPromiseAdapter();
+const athleteRepository = new AthleteRepositoryDatabase(connection);
+const registerAthlete = new RegisterAthlete(athleteRepository)
+new AthleteController(httpServer, registerAthlete);
 httpServer.listen(5000);
