2853a5a3ec992608b170c1187bffb13a09766a69
Matheus Sena <senamatheusjob@gmail.com>
2024-11-02 00:57:59 -0300

    refactor: Add validation for minimum length in Name value object constructor

--- src/Athlete/domain/vo/Name.ts
+++ src/Athlete/domain/vo/Name.ts
@@ -2,6 +2,7 @@ export default class Name {
     private value: string;
 
     constructor(name: string) {
+        if (name.length < 3) throw new Error('Name must have at least 3 characters');
         if (!name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error('Invalid name');
         this.value = name;
     }
