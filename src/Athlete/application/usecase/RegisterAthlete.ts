dfcdfaa116fc9476a6d3ee064542f952ee76c75f
Matheus Sena <senamatheusjob@gmail.com>
2024-11-02 00:07:28 -0300

    feat: Add RegisterAthlete use case

---
+++ src/Athlete/application/usecase/RegisterAthlete.ts
@@ -0,0 +1,26 @@
+import UseCase from '../../../shared/application/usecase/UseCase';
+import Athlete from '../../domain/entity/Athlete';
+import AthleteRepository from '../repository/AthleteRepository';
+
+export default class RegisterAthlete implements UseCase {
+    constructor(readonly athleteRepository: AthleteRepository) { }
+
+    async execute(input: Input): Promise<Output> {
+        const existingAthlete = await this.athleteRepository.getByCpf(input.cpf);
+        if (existingAthlete) throw new Error('Athlete already exists');
+        const athlete = Athlete.create(input.name, input.cpf, +input.age);
+        await this.athleteRepository.save(athlete);
+        return { athleteId: athlete.id };
+    }
+}
+
+
+type Input = {
+    name: string,
+    cpf: string,
+    age: string,
+}
+
+type Output = {
+    athleteId: string
+}
