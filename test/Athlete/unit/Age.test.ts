9623d831ba6595cb3d8d00d03ae230b8b8567f2b
Matheus Sena <senamatheusjob@gmail.com>
2024-11-02 00:08:33 -0300

    test: Add unit tests for Age value object

---
+++ test/Athlete/unit/Age.test.ts
@@ -0,0 +1,40 @@
+import Age from '../../../src/Athlete/domain/vo/Age';
+
+test.each([
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27
+])('Should create an valid age with value %s', function (age: number) {
+    expect(() => new Age(age)).toBeDefined();
+})
+
+test.each([
+    17,
+    2,
+    1,
+    -6,
+])('Should not create an valid age with value %s', function (age: number) {
+    expect(() => new Age(age)).toThrow(new Error('Minor cant compete'));
+})
+
+test.each([
+    101,
+    102,
+    103,
+    104,
+    105,
+    106,
+    107,
+    108,
+    109,
+    110
+])('Should not create an valid age with value %s', function (age: number) {
+    expect(() => new Age(age)).toThrow(new Error('Elderly cant compete'));
+})
