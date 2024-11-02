e8f30328bdee1bbe29baddefa5d1b9856ecad14e
Matheus Sena <senamatheusjob@gmail.com>
2024-11-02 00:07:41 -0300

    feat: Add Age value object for Athlete domain

---
+++ src/Athlete/domain/vo/Age.ts
@@ -0,0 +1,32 @@
+export default class Age {
+    private value: number;
+
+    constructor(age: number) {
+        this.validateAge(age);
+        this.value = age;
+    }
+
+    validateAge(age: number) {
+        if (!age) throw new Error('Age is required');
+        if (this.isMinor(age)) throw new Error('Minor cant compete');
+        if (this.isElderly(age)) throw new Error('Elderly cant compete');
+        return true;
+    }
+
+
+    getValue() {
+        return this.value;
+    }
+
+    setValue(age: number) {
+        this.value = new Age(age).getValue();
+    }
+
+    isMinor(age: number) {
+        return age < 18;
+    }
+
+    isElderly(age: number) {
+        return age > 100;
+    }
+}
