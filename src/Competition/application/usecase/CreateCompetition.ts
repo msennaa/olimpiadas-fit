17da531ac573b8d8e5f996c58351d2621c2abe9b
Matheus Sena <senamatheusjob@gmail.com>
2024-11-03 10:11:15 -0300

    feat: Implement CreateCompetition use case

---
+++ src/Competition/application/usecase/CreateCompetition.ts
@@ -0,0 +1,26 @@
+import UseCase from '../../../shared/application/usecase/UseCase';
+import Competition from '../../domain/entity/Competition';
+import CompetitionType from '../../domain/entity/CompetitionType';
+import CompetitionRepository from '../repository/CompetitionRepository';
+import CompetitionTypeRepository from '../repository/CompetitionTypeRepository';
+
+export default class CreateCompetition implements UseCase {
+    constructor(readonly competitionRepository: CompetitionRepository, readonly competitionTypeRepository: CompetitionTypeRepository) { }
+
+    async execute(input: Input): Promise<Output> {
+        const existingCompetitionType = await this.competitionTypeRepository.getById(input.competitionTypeId);
+        if (!existingCompetitionType) throw new Error('Competition type not found');
+        const competition = Competition.create(input.name, input.competitionTypeId);
+        await this.competitionRepository.save(competition);
+        return { competitionId: competition.id }
+    }
+}
+
+type Input = {
+    name: string;
+    competitionTypeId: string;
+}
+
+type Output = {
+    competitionId: string;
+}
