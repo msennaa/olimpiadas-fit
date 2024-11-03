96ec6015c05a26ea615a3a28f2a2d33da3d6665c
Matheus Sena <senamatheusjob@gmail.com>
2024-11-03 10:12:32 -0300

    refactor: Update Competition.test.ts to use getName() method for name comparison

--- test/Competition/unit/Competition.test.ts
+++ test/Competition/unit/Competition.test.ts
@@ -3,7 +3,7 @@ import Competition from '../../../src/Competition/domain/entity/Competition'
 test('Should create an competition successfully', function () {
     const competition = Competition.create('any_competition', 'any_competition_type_id');
     expect(competition.id).toBeDefined();
-    expect(competition.name).toBe('any_competition');
+    expect(competition.getName()).toBe('any_competition');
     expect(competition.getStatus()).toBe('in-progress');
     expect(competition.startCompetition).toBeDefined();
     expect(competition.endCompetition).toBeNull();
