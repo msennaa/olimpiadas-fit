899be474b822f8a613fd367ae175a76f6ec0b510
Matheus Sena <senamatheusjob@gmail.com>
2024-11-03 11:10:01 -0300

    feat: Implement FinishCompetition use case

---
+++ src/Competition/application/usecase/FinishCompetition.ts
@@ -0,0 +1,22 @@
+import UseCase from '../../../shared/application/usecase/UseCase';
+import Competition from '../../domain/entity/Competition';
+import CompetitionRepository from '../repository/CompetitionRepository';
+
+export default class FinishCompetition implements UseCase {
+    constructor(readonly competitionRepository: CompetitionRepository) { }
+
+    async execute(competitionId: string): Promise<Output> {
+        const competition = await this.competitionRepository.getById(competitionId);
+        competition.finishCompetition();
+        await this.competitionRepository.updateCompetition(competition);
+        return {
+            competitionId: competition.id,
+            status: competition.getStatus()
+        }
+    }
+}
+
+type Output = {
+    competitionId: string;
+    status: string
+}
