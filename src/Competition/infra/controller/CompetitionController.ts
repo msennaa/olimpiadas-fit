21024c971f1ee0c8f0df7ecec1cd0035ad77fb88
Matheus Sena <senamatheusjob@gmail.com>
2024-11-03 11:10:22 -0300

    feat: Add FinishCompetition endpoint to CompetitionController

--- src/Competition/infra/controller/CompetitionController.ts
+++ src/Competition/infra/controller/CompetitionController.ts
@@ -1,10 +1,11 @@
 import HttpServer from '../../../shared/application/http/HttpServer';
 import CreateCompetition from '../../application/usecase/CreateCompetition';
+import FinishCompetition from '../../application/usecase/FinishCompetition';
 import GetCompetitionById from '../../application/usecase/GetCompetitionById';
 
 export default class CompetitionController {
 
-    constructor(readonly httpServer: HttpServer, readonly createCompetition: CreateCompetition, readonly getCompetitionById: GetCompetitionById) {
+    constructor(readonly httpServer: HttpServer, readonly createCompetition: CreateCompetition, readonly getCompetitionById: GetCompetitionById, readonly finishCompetition: FinishCompetition) {
         this.httpServer?.register('post', "/competition", async (params: any, body: any) => {
             const response = await this.createCompetition.execute(body);
             return response;
@@ -13,6 +14,12 @@ export default class CompetitionController {
         this.httpServer?.register('get', '/competition/:{competitionId}', async (params: any, body: any) => {
             const competitionId = params.competitionId;
             const output = await this.getCompetitionById.execute(competitionId);
+            return output;
+        })
+
+        this.httpServer?.register('put', '/competition/finished/:{competitionId}', async (params: any, body: any) => {
+            const competitionId = params.competitionId;
+            const output = await this.finishCompetition.execute(competitionId);
             return output;
         })
     }
