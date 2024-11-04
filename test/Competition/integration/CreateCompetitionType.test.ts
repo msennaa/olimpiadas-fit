8ec666ea4386555f788a1f76174390c983bac5b3
Matheus Sena <senamatheusjob@gmail.com>
2024-11-04 08:31:48 -0300

    feat: Add CreateCompetitionType use case for creating competition types

--- test/Competition/integration/CreateCompetitionType.test.ts
+++ test/Competition/integration/CreateCompetitionType.test.ts
@@ -1,22 +1,40 @@
 import CreateCompetitionType from '../../../src/Competition/application/usecase/CreateCompetitionType';
+import CompetitionTypeRepositoryDatabase from '../../../src/Competition/infra/repository/database/CompetitionTypeRepositoryDatabase';
 import CompetitionTypeRepositoryInMemory from '../../../src/Competition/infra/repository/in-memory/CompetitionTypeRepositoryInMemory';
+import DatabaseConnection from '../../../src/shared/application/database/DatabaseConnection';
+import { PgPromiseAdapter } from '../../../src/shared/infra/database/PgPromiseAdapter';
 
-let createCompetitionType: CreateCompetitionType;
+let connection: DatabaseConnection;
+let createCompetitionTypeInMemory: CreateCompetitionType;
+let createCompetitionTypeDatabase: CreateCompetitionType;
 
 beforeEach(async () => {
+    connection = new PgPromiseAdapter(5433);
+    await connection.query('DELETE FROM competition_type', []);
     const competitionTypeRepository = new CompetitionTypeRepositoryInMemory();
-    createCompetitionType = new CreateCompetitionType(competitionTypeRepository);
+    const competitionTypeRepositoryDatabase = new CompetitionTypeRepositoryDatabase(connection);
+    createCompetitionTypeInMemory = new CreateCompetitionType(competitionTypeRepository);
+    createCompetitionTypeDatabase = new CreateCompetitionType(competitionTypeRepositoryDatabase);
 })
 
 test('Should create a competition type successfully', async function () {
     const inputCreateCompetitionTypeOutput = { name: 'any competition type' }
-    const createCompetitionTypeOutput = await createCompetitionType.execute(inputCreateCompetitionTypeOutput)
+    const createCompetitionTypeOutput = await createCompetitionTypeInMemory.execute(inputCreateCompetitionTypeOutput)
     expect(createCompetitionTypeOutput.competitionTypeId).toBeDefined();
 })
 
 test('Should not create a competition type if already exists another competition with same name', async function () {
     const inputCreateCompetitionTypeOutput = { name: 'any competition type' }
-    await createCompetitionType.execute(inputCreateCompetitionTypeOutput)
-    await expect(createCompetitionType.execute(inputCreateCompetitionTypeOutput)).rejects.toThrow(new Error('Competition Type already exists'));
+    await createCompetitionTypeDatabase.execute(inputCreateCompetitionTypeOutput)
+    await expect(createCompetitionTypeDatabase.execute(inputCreateCompetitionTypeOutput)).rejects.toThrow(new Error('Competition Type already exists'));
 })
 
+test('Should not create a competition type if already exists another competition with same name', async function () {
+    const inputCreateCompetitionTypeOutput = { name: 'any competition type' }
+    await createCompetitionTypeInMemory.execute(inputCreateCompetitionTypeOutput)
+    await expect(createCompetitionTypeInMemory.execute(inputCreateCompetitionTypeOutput)).rejects.toThrow(new Error('Competition Type already exists'));
+})
+
+afterEach(async () => {
+    connection.close();
+})
