f5e98257707dd2b9aa3136ea3e16f84682c5f156
Matheus Sena <senamatheusjob@gmail.com>
2024-11-05 05:23:43 -0300

    refactor: ensure getById return a competitionType

--- src/Competition/application/repository/CompetitionTypeRepository.ts
+++ src/Competition/application/repository/CompetitionTypeRepository.ts
@@ -2,6 +2,6 @@ import CompetitionType from '../../domain/entity/CompetitionType';
 
 export default interface CompetitionTypeRepository {
     save(competitionType: CompetitionType): Promise<void>;
-    getById(competitionTypeId: string): Promise<CompetitionType | null>;
+    getById(competitionTypeId: string): Promise<CompetitionType>;
     getByName(competitionName: string): Promise<CompetitionType | null>;
 }
