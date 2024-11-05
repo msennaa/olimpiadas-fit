2d8cbaa6873d378322a18dd4eb858a9866031324
Matheus Sena <senamatheusjob@gmail.com>
2024-11-05 05:22:27 -0300

    refactor: ensure getById return athlete

--- src/Athlete/application/repository/AthleteRepository.ts
+++ src/Athlete/application/repository/AthleteRepository.ts
@@ -2,6 +2,6 @@ import Athlete from '../../domain/entity/Athlete';
 
 export default interface AthleteRepository {
     save(athlete: Athlete): Promise<void>;
-    getById(athleteId: string): Promise<Athlete | null>;
+    getById(athleteId: string): Promise<Athlete>;
     getByCpf(cpf: string): Promise<Athlete | null>;
 }
