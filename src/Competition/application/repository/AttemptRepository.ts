b6fc83196ad1ee80b905838d1dcec2311704a3ab
Matheus Sena <senamatheusjob@gmail.com>
2024-11-08 21:44:42 -0300

    feat: Add getRanking method to AttemptRepository

--- src/Competition/application/repository/AttemptRepository.ts
+++ src/Competition/application/repository/AttemptRepository.ts
@@ -3,4 +3,5 @@ import Attempt from '../factory/Attempt';
 export default interface AttemptRepository {
     save(attempt: Attempt): Promise<void>;
     allowedToAttempt(athleteId: string, competitionId: string, competitionName: string): Promise<boolean>;
+    getRanking(competitionId: string, page: number, pageSize: number, competitionType: string): Promise<any[]>;
 }
