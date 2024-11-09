fda160e5207f712eb2920b816f7a76b575bad7c5
Matheus Sena <senamatheusjob@gmail.com>
2024-11-08 21:44:49 -0300

    feat: Implement GetRanking use case

---
+++ src/Competition/application/usecase/GetRanking.ts
@@ -0,0 +1,29 @@
+import UseCase from '../../../shared/application/usecase/UseCase';
+import AttemptRepository from '../repository/AttemptRepository';
+import CompetitionRepository from '../repository/CompetitionRepository';
+
+export default class GetRanking implements UseCase {
+    constructor(readonly attemptRepository: AttemptRepository, readonly competitionRepository: CompetitionRepository, readonly competitionTypeRepository: any) { }
+
+    async execute(input: Input): Promise<Output[]> {
+        const competition = await this.competitionRepository.getById(input.competitionId);
+        const competitionType = await this.competitionTypeRepository.getById(competition.competitionTypeId);
+        input.page = input.page || 1;
+        input.limit = input.limit || 10;
+        const ranking = await this.attemptRepository.getRanking(competition.id, input.page, input.limit, competitionType.getName());
+        return ranking;
+    }
+}
+
+type Input = {
+    competitionId: string;
+    page?: number;
+    limit?: number;
+}
+
+type Output = {
+    position: number,
+    athlete: string,
+    unit: string,
+    value: number
+} 
