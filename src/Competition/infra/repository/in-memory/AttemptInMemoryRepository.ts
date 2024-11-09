086bb3eaf2456d87ba4f82a44c9d9f338ec39e0e
Matheus Sena <senamatheusjob@gmail.com>
2024-11-08 21:44:58 -0300

    feat: Add getRanking method to AttemptInMemoryRepository

--- src/Competition/infra/repository/in-memory/AttemptInMemoryRepository.ts
+++ src/Competition/infra/repository/in-memory/AttemptInMemoryRepository.ts
@@ -20,4 +20,38 @@ export default class AttemptInMemoryRepository implements AttemptRepository {
         const attempts = this.attempt.filter((attempt: Attempt) => attempt.athleteId === athleteId && attempt.competitionId === competitionId);
         return competitionName === 'dart' ? attempts.length < this.maxDartAttempt : attempts.length < this.maxRestAttempt;
     }
+
+    async getRanking(competitionId: string, page: number, pageSize: number, competitionType: string): Promise<any[]> {
+        const filteredAttempts = this.attempt.filter(attempt => attempt.competitionId === competitionId && attempt.getDeletedAt() === null);
+        const processedAttempts = filteredAttempts.map(attempt => {
+            let baseValue = attempt.getValue();
+            if (attempt.getUnit() === 'ml' || attempt.getUnit() === 'mg') {
+                baseValue = attempt.getValue() / 1000;
+            } else if (attempt.getUnit() === 's') {
+                baseValue = attempt.getValue() / 60;
+            } else if (attempt.getUnit() === 'cm') {
+                baseValue = attempt.getValue() / 100;
+            }
+            return {
+                athleteId: attempt.athleteId,
+                athleteName: 'any_name',
+                value: attempt.getValue(),
+                unit: attempt.getUnit(),
+                baseValue,
+            };
+        });
+        const distinctAttempts = processedAttempts.filter((value, index, self) =>
+            index === self.findIndex((t) => t.athleteId === value.athleteId)
+        );
+        const sortedAttempts = distinctAttempts.sort((a, b) => b.baseValue - a.baseValue);
+        const attemptsWithRowNumber = sortedAttempts.map((attempt, index) => ({
+            position: index + 1,
+            athlete: attempt.athleteName,
+            value: attempt.value,
+            unit: attempt.unit,
+        }));
+        const offset = (page - 1) * pageSize;
+        const paginatedAttempts = attemptsWithRowNumber.slice(offset, offset + pageSize);
+        return paginatedAttempts;
+    }
 }
