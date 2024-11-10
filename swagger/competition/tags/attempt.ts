4abcb01fcca957e7c3a2f7495af5cec91946d6f0
Matheus Sena <senamatheusjob@gmail.com>
2024-11-10 10:09:01 -0300

    feat: Add attempt tag for documentation

---
+++ swagger/competition/tags/attempt.ts
@@ -0,0 +1,8 @@
+export const attemptTag = {
+    name: "Attempt",
+    description: "Operações de tentativas",
+    // externalDocs: {
+    //     description: "Mais informações",
+    //     url: "http://seusite.com",
+    // },
+}
