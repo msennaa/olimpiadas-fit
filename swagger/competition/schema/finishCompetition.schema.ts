2073775db712d5f707780fd08954f499e2132144
Matheus Sena <senamatheusjob@gmail.com>
2024-11-10 10:08:49 -0300

    feat: Add finishCompetition schema and routes for API documentation

---
+++ swagger/competition/schema/finishCompetition.schema.ts
@@ -0,0 +1,39 @@
+export const finishCompetition = {
+    put: {
+        tags: ["Competition"],
+        summary: "Finalizar uma competição",
+        description: "Finaliza uma competição",
+        produces: ["application/json"],
+        parameters: [
+            {
+                name: "id",
+                in: "path",
+                description: "ID da Competição",
+                required: true,
+                type: "string",
+                format: "uuid",
+            },
+        ],
+        responses: {
+            "200": {
+                description: "Operação bem-sucedida",
+                schema: {
+                    type: "object",
+                    properties: {
+                        competitionId: { type: "string" },
+                        status: { type: "string" },
+                    },
+                },
+            },
+            "422": {
+                description: "Competição não encontrada",
+                schema: {
+                    type: "object",
+                    properties: {
+                        message: { type: "string" },
+                    },
+                },
+            },
+        },
+    },
+}
