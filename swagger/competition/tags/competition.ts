6f6e33339c5ad0546b404560d4072798a5c9ce2e
Matheus Sena <senamatheusjob@gmail.com>
2024-11-10 09:21:31 -0300

    feat: Add competition tag for documentation

---
+++ swagger/competition/tags/competition.ts
@@ -0,0 +1,8 @@
+export const competitionTag = {
+    name: "Competition",
+    description: "Operações de competition",
+    // externalDocs: {
+    //     description: "Mais informações",
+    //     url: "http://seusite.com",
+    // },
+}
