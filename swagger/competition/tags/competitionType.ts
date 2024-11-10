04177cc188b619ca97a51a1d1a8f43b8698c9731
Matheus Sena <senamatheusjob@gmail.com>
2024-11-10 10:09:05 -0300

    feat: Add competition type tag for documentation

---
+++ swagger/competition/tags/competitionType.ts
@@ -0,0 +1,8 @@
+export const competitionTypeTag = {
+    name: "CompetitionType",
+    description: "Operações de competitionType",
+    // externalDocs: {
+    //     description: "Mais informações",
+    //     url: "http://seusite.com",
+    // },
+}
