f28f8745390dfe8cbf0437aa2cbc7bf54a7acbff
Matheus Sena <senamatheusjob@gmail.com>
2024-11-10 09:21:07 -0300

    feat: add athlete tag for documentation

---
+++ swagger/athletes/tags/athletes.ts
@@ -0,0 +1,8 @@
+export const athleteTag = {
+    name: "Athlete",
+    description: "Operações de Athlete",
+    // externalDocs: {
+    //     description: "Mais informações",
+    //     url: "http://seusite.com",
+    // },
+}
