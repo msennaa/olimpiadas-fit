cc79302b0d6215c59064d281dae33088bc7347c9
Matheus Sena <senamatheusjob@gmail.com>
2024-11-10 10:52:51 -0300

    feat: Add ValidationError class for handling validation errors

---
+++ src/shared/domain/errors/ValidationError.ts
@@ -0,0 +1,9 @@
+export class ValidationError extends Error {
+    status: number;
+
+    constructor(message: string) {
+        super(message);
+        this.name = "ValidationError";
+        this.status = 422;
+    }
+}
