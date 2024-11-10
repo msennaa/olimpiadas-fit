80151c869045a31b983921b315509caa785b93a4
Matheus Sena <senamatheusjob@gmail.com>
2024-11-10 10:59:24 -0300

    feat: Add BadRequestError class for handling 400 errors

---
+++ src/shared/domain/errors/BadRequestError.ts
@@ -0,0 +1,9 @@
+export class BadRequestError extends Error {
+    status: number;
+
+    constructor(message: string) {
+        super(message);
+        this.name = 'BadRequestError';
+        this.status = 400;
+    }
+}
