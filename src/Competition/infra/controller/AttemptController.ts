bfccd7ca1b2304aad9f7ff6c1585b5b8c51115de
Matheus Sena <senamatheusjob@gmail.com>
2024-11-15 23:54:15 -0300

    feat: Add queue functionality to AttemptController

--- src/Competition/infra/controller/AttemptController.ts
+++ src/Competition/infra/controller/AttemptController.ts
@@ -1,10 +1,11 @@
 import HttpServer from '../../../shared/application/http/HttpServer';
+import Queue from '../../../shared/application/Queue/Queue';
 import CreateAttempt from '../../application/usecase/CreateAttempt';
 import getRanking from '../../application/usecase/GetRanking';
 
 export default class AttemptController {
 
-    constructor(readonly httpServer: HttpServer, readonly createAttempt: CreateAttempt, readonly getRanking: getRanking) {
+    constructor(readonly httpServer: HttpServer, readonly createAttempt: CreateAttempt, readonly getRanking: getRanking, readonly queue: Queue) {
         this.httpServer?.register('post', '/attempt/:{competitionId}', async (params: any, body: any) => {
             const attemptData = Object.assign(params, body);
             const output = await this.createAttempt.execute(attemptData);
@@ -15,6 +16,12 @@ export default class AttemptController {
             const input = { competitionId: params.competitionId, page: query.page, limit: query.limit };
             const output = await this.getRanking.execute(input);
             return output;
+        })
+
+
+        this.httpServer?.register('post', '/async_attempt/:{competitionId}', async (params: any, body: any) => {
+            const input = body;
+            await this.queue.publish('competition', input);
         })
     }
 }
